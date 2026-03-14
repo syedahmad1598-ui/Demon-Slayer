bbe5ae048c6da1189c5d73959ae7b73a081bf6f7
syedahmad1598-ui <syedahmad1598@gmail.com>
2026-03-15 00:27:41 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,14 +1,12 @@
 from flask import Flask, render_template
 import os
 
-app = Flask(__name__, template_folder='.')
+app = Flask(__name__, template_folder='.', static_folder='static')
 
 @app.route('/')
 def home():
-    # This looks for index.html in your folder
     return render_template('index.html')
 
 if __name__ == "__main__":
-    # Render uses a specific port, this helps it find it
     port = int(os.environ.get("PORT", 5000))
     app.run(host='0.0.0.0', port=port)
